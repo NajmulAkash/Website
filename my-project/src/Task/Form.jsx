14431603934b2890cbf1bfc1b5879c759e599d84
Najmul Akash <najmulbalghari23@gamil.com>
2026-05-14 08:17:33 -0700

    Strick Note

--- my-project/src/Task/Form.jsx
+++ my-project/src/Task/Form.jsx
@@ -1,11 +1,22 @@
+import { RiCloseCircleFill } from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosAdd } from "react-icons/io";
 function Form (){
     return(
         <>
-        <div className="w-full h-screen bg-gray-500 flex justify-center">
-            <div className="w-96 h-2/3 bg-red-500 my-32 rounded-md">
-            <input type="text" placeholder="Enter your name" className="outline-2 rounded-md w-80 h-6 px" />
-            </div>
+      <div className="flex p-5 flex-row bg-gray-600">
+        <div className="w-32 h-full outline-2 mr-2 bg-amber-50">
+            <div className="toolbar flex justify-between">
+            <button className="hover:bg-amber-100 cursor-pointer w-6 h-6 flex justify-center items-center"><IoIosAdd /></button>
+            <button className="hover:bg-amber-100 cursor-pointer w-6 h-6 flex justify-center items-center"><IoSettingsOutline /></button>
+            <button className="hover:bg-amber-100 cursor-pointer w-6 h-6 flex justify-center items-center"><RiCloseCircleFill /></button>
         </div>
+            <h1>Sticky Notes</h1>
+        </div>
+        <div className="w-full outline-2">
+            <h1>Notes View</h1>
+        </div>
+      </div>
         </>
     )
 }
